c5ca025ad67e238e040b9a6efbb86ee789005020
Howie <howieforjob@163.com>
2020-03-01 11:45:48 +0800

    update header 组件更新

--- src/components/public/PageHeader.jsx
+++ src/components/public/PageHeader.jsx
@@ -2,20 +2,21 @@ import React, { Component } from 'react'
 import styles from './style.module.css'
 import { switchLanguage } from '../../redux/action'
 import { mapStateToProps } from '../../redux/store'
-import menuIcon from '../../assets/imgs/menu.svg'
 import { connect } from 'react-redux'
+
+import drawerBtn from '../../assets/imgs/drawerBtn.png'
+import headerLogo from '../../assets/imgs/headerLogo.png'
 
 class PageHeader extends Component {
   render() {
     const { dispatch, currentLanguage } = this.props;
     return (
       <div className={styles.pageHeader}>
-        <div className={styles.left}>
-          <img src={menuIcon} alt="" />
-        </div>
-        <div className={styles.center}></div>
-        <div className={styles.right}
-          onClick={() => dispatch(switchLanguage(currentLanguage))}>{currentLanguage}</div>
+        <img className={styles.leftPic} src={drawerBtn} alt=""/>
+        <img className={styles.centerLogo} src={headerLogo} alt=""/>
+        <div 
+        className={styles.rightWords}
+        onClick={() => dispatch(switchLanguage(currentLanguage))}>{currentLanguage}</div>
       </div>
     )
   }
