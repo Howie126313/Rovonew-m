f737b86ce70323c23fa7d8399c54d2d03a26e815
Howie126313 <593766927@qq.com>
2020-03-14 10:31:01 +0800

    update

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -14,21 +14,27 @@ class SideItem extends Component {
     const array = [
       {
         name: 'HOME',
+        zhName: '首页',
         path: '/'
       },
       {
         name: 'ABOUT',
+        zhName: '关于我们',
         path: '/about'
       },
       {
         name: 'JOIN US',
+        zhName: '加入我们',
         path: '/joinus'
       },
       {
         name: 'CONTACT',
+        zhName: '联系我们',
         path: '/contact'
       }
     ]
+
+    const { currentLanguage } = this.props
 
     const sidebar = (
       array.map((item, index) => {
@@ -39,7 +45,7 @@ class SideItem extends Component {
           key={index}
           onClick={() => {this.props.dispatch(changeDrawerstate(this.props.drawerstate))}}>
             <div className={style.sideItem}>
-              <span>{item.name}</span>
+              <span>{currentLanguage === 'EN' ? item.name : item.zhName}</span>
               { item.path === window.location.hash.replace('#', '') ? null : <img src={arrow} alt=""/>}
             </div>
           </Link>
@@ -53,7 +59,7 @@ class SideItem extends Component {
           <img className={style.close} src={closeBBtn}  onClick={this.close.bind(this)} alt=""/>
         </div>
         <div className={style.searchContainer}>
-          <input type="text" placeholder="搜索..." />
+          <input type="text" placeholder={currentLanguage === 'EN' ? 'Search...' : '搜索...'} />
           <img src={magnifying} alt=""/>
         </div>
         {sidebar}
